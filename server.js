1954ad1e50f22466f8ee52d5132e7a0fa9f76c05
khushii-13 <khushichaudhary904@gmail.com>
2026-03-24 13:35:11 +0530

    setup mongodb

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const app = express();
+dotenv.config();
+const HOST = process.env.HOST;
+const PORT = process.env.PORT;
+const MONGO_URL = process.env.MONGO_URL;
+app.get("/", (req, res) => {
+  const h1 = "<h1>Hello everyone</h1>";
+  res.send(h1);
+});
+app.get("/home", (req, res) => {
+  const h1 = "<h1>Hello Home</h1>";
+  res.send(h1);
+});
 
-app.get("/",(req, res)=>{
-    const h1 = "<h1>Hello everyone</h1>";
-    res.send(h1);
-})
-
-app.listen(8000, ()=>{
-    console.log(`Server is listening on http://localhost:8000`);
-})
+const startServer = async () => {
+  try {
+    await mongoose.connect(MONGO_URL);
+    app.listen(8000, () => {
+      console.log(`Server is listening on http://${HOST}:${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+startServer();

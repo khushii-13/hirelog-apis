8524897270f102430adb2ddd3c8f45f7e191989d
khushii-13 <khushichaudhary904@gmail.com>
2026-03-26 13:51:21 +0530

    implemented register api

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,56 @@
-const register = (req, res)=>{
-res.status(200).json({
-    message : "user registered"
-})
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+
+const register = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    if (!name || !email || !password || !role) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    }
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    await User.create({
+      name,
+      email,
+      password: hashPassword,
+      role,
+    });
+
+    return res.status(201).json({
+      message: "User created successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Server error",
+      error: error.message,
+    });
+  }
+};
+
+const login = async (req, res) =>{
+ return res.status(200).json({
+    message  : "Login hitted succesfully",
+    htoken : "abc1237kkk"
+ })
 }
-module.exports = {register};
+
+const getUser = async (req,res)=>{
+    
+    return res.status(200).json({
+    message  : "Get user hitted succesfully",
+    data : req.headers.htoken
+ })
+}
+module.exports = { register, login, getUser};
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const authRouter = express.Router();
-const {register} = require("../controllers/authController");
+const {register, login, getUser} = require("../controllers/authController");
 
 authRouter.post("/register", register);
+authRouter.post("/login",login);
+authRouter.get("/get-user",getUser);
 
 module.exports = authRouter;
